510b8ee5e7f95b12a13375ab5d29c68bbf63c0cf
radion-x <radion@websited.org>
2025-11-30 21:11:11 +1100

    Fix: Correct import syntax in DoctorDashboard.tsx

--- client/src/components/DoctorDashboard.tsx
+++ client/src/components/DoctorDashboard.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';\nimport { getApiUrl } from '../config/api';
+import React, { useState, useEffect } from 'react';
+import { getApiUrl } from '../config/api';
 
 // Define interfaces for our data structures
 interface User {
